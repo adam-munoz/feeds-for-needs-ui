3c38f34a9647b6f649fc35b032892d6d6a3b362b
coolchoots <smartswater@gmail.com>
2020-05-30 15:46:34 -0500

    Remove future components. For now I will focus on MVP.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AchieverComponent } from './achiever/achiever.component';
 import { FeedingPlanComponent } from './feeding-plan/feeding-plan.component';
-import { LifestyleComponent } from './lifestyle/lifestyle.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AchieverComponent,
-    FeedingPlanComponent,
-    LifestyleComponent
+    FeedingPlanComponent
   ],
   imports: [
     BrowserModule,
